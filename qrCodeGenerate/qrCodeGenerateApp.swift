0d32fb6aa0cd3f6ec338c99fb83942ae340f678e
AlexxalexS <37020080+AlexxalexS@users.noreply.github.com>
2021-10-28 22:35:46 +0300

    Initial Commit

---
+++ qrCodeGenerate/qrCodeGenerateApp.swift
@@ -0,0 +1,17 @@
+//
+//  qrCodeGenerateApp.swift
+//  qrCodeGenerate
+//
+//  Created by Alexey on 28.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct qrCodeGenerateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
